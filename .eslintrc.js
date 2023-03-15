6b61a02cc85b20c98070db632800d933ba2d1d73
DanMendiz <114294487+DanMendiz@users.noreply.github.com>
2023-03-15 00:01:39 +0000

    initial-files

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "files": ['**/*.js'],
+    "env": {
+        "es2021": true
+    },
+    "extends": "airbnb-base",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        curly: "off"
+    }
+}
---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+/* global _ */
+export default function bad(n) {
+  // eslint-disable-next-line no-unused-vars
+  const result = _.chunk(['a', 'b', 'c']);
+  return n === 2;
+}
